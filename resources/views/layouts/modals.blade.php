8bdb00f10a456c5eaee57efc52d831dc004fc596
Nasrodin Amatonding <nasrodin1@gmail.com>
2022-01-30 16:51:47 +0800

    update

--- resources/views/layouts/modals.blade.php
+++ resources/views/layouts/modals.blade.php
@@ -55,83 +55,22 @@ $contact = null;
               </div>
             </form>
             <div class="row mg0" style="margin-top: 30% !important;">
-              {{-- <button class="btn" id="checkout">Checkout</button> --}}
               <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
                 <input type="hidden" name="cmd" value="_xclick"> 
                 <input type="hidden" name="business" value="sb-mxcjx12110920@business.example.com"> 
                 <input type="hidden" name="item_name" value="Jordan 1 Retro High"> 
                 <input type="hidden" name="item_number" value="123"> 
-                <input type="hidden" name="amount" value="1"> 
+                <input type="hidden" name="amount" value="1800"> 
                 <input type="hidden" name="address_override" value="1"> 
                 <input type="hidden" name="first_name" value="{{ $firstName }}"> 
                 <input type="hidden" name="last_name" value="{{ $lastName }}"> 
                 <input type="hidden" name="address1" value="{{ $address }}"> 
                 <input type="hidden" name="night_phone_a" value="{{ $contact }}"> 
-                {{-- <input type="hidden" name="night_phone_b" value="555">  --}}
-                {{-- <input type="hidden" name="night_phone_c" value="1234">  --}}
                 <input type="hidden" name="email" value="{{ $email }}"> 
-                {{-- <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" alt="PayPal - The safer, easier way to pay online"> --}}
                 <input class="btn" id="checkout" type="submit" value="Buy Now">
               </form>
             </div>
           </div>
-          {{-- <div class="info-container" id="info">
-            <div class="row" style="height: 45px"></div>
-            <div class="row mg0">
-              <div class="col m6" style="padding: 0px 20px">
-                <form>
-                  <div class="row mg0 mgb10">
-                    <div class="col m6 pd0">
-                      <input type="text" name="text-1" placeholder="First Name">
-                    </div>
-                    <div class="col m6 pd0">
-                      <input type="text" name="text-2" placeholder="Last Name">
-                    </div>
-                  </div>
-                  <div class="row mg0 mgb10">
-                    <input type="email" name="email-1" placeholder="E-mail Address">
-                  </div>
-                  <div class="row mg0 mgb10">
-                    <input type="text" name="text-3" placeholder="Address">
-                  </div>
-                  <div class="row mg0 mgb10">
-                    <input type="text" name="text-4" placeholder="Contact">
-                  </div>
-                  <div class="row mg0 mgb10">
-                    <h3 style="font-size: 16px">Payment Method</h3>
-                    <label>
-                      <input name="group1" type="radio" checked />
-                      <span style="color: black">Paypal</span>
-                    </label>
-                  </div>
-                </form>
-                <div class="row mg0" style="margin-top: 30% !important;">
-                  <button class="btn" id="checkout">Checkout</button>
-                </div>
-              </div>
-              <div class="col m6 pd0">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                  <input type="hidden" name="cmd" value="_xclick"> 
-                  <input type="hidden" name="business" value="sb-mxcjx12110920@business.example.com"> 
-                  <input type="hidden" name="item_name" value="Jordan 1 Retro High"> 
-                  <input type="hidden" name="item_number" value="123"> 
-                  <input type="hidden" name="amount" value="1800"> 
-                  <input type="hidden" name="first_name" value="John"> 
-                  <input type="hidden" name="last_name" value="Doe"> 
-                  <input type="hidden" name="address1" value="9 Elm Street"> 
-                  <input type="hidden" name="address2" value="Apt 5"> 
-                  <input type="hidden" name="city" value="Berwyn"> 
-                  <input type="hidden" name="state" value="US"> 
-                  <input type="hidden" name="zip" value="19312"> 
-                  <input type="hidden" name="night_phone_a" value="4083792928"> 
-                  <input type="hidden" name="night_phone_b" value="555"> 
-                  <input type="hidden" name="night_phone_c" value="1234"> 
-                  <input type="hidden" name="email" value="jdoe@zyzzyu.com"> 
-                  <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" alt="PayPal - The safer, easier way to pay online">
-                </form>
-              </div>
-            </div> 
-          </div> --}}
         </div>
       </div>
     </div>
